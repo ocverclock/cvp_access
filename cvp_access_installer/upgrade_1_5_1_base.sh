dfa0b109026186dd99ca42b69e2a3d2ed86eba96
ocverclock <45900936+ocverclock@users.noreply.github.com>
2026-09-24 16:15:49 +0200

    Require Wi-Fi connector in 1.5.1 upgrade

--- cvp_access_installer/upgrade_1_5_1_base.sh
+++ cvp_access_installer/upgrade_1_5_1_base.sh
@@ -35,6 +35,7 @@ required=(
     cvp_access_installer/tools/cvp_web.py
     cvp_access_installer/install_maintenance.sh
     cvp_access_installer/network/cvp-wifi-fallback
+    cvp_access_installer/network/cvp-wifi-connect
     cvp_access_installer/systemd/cvp-wifi-fallback.service.in
     cvp_access_installer/systemd/cvp-web.service.in
 )
